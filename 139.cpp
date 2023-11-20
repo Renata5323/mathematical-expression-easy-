168116152f745c61f7274ddb95719838fd781b6a
Renata <opel5323@gmail.com>
2023-11-20 17:42:31 +0300

    139.cpp

---
+++ 139.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+setlocale(0," "); using namespace  std;
+cout<<"Введите число 1: "; int a; cin>>a;  
+cout<<"Введите число 2: "; int b; cin>>b;
+cout<<"Введите число 3: "; int f; cin>>f;    
+int q=(a+b-f/a)+f*a*a-(a+b);cout<<q;}
